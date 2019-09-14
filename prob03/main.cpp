2e154f412d8bec9f7c808bb12eb40ecd2d845cbc
BaDunky <arturitosilva20@hotmail.com>
2019-09-14 13:01:18 -0700

    added comments

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -9,8 +9,8 @@ int main(){
   std::cout << "Please enter the person's height in inches: ";
   std::cin >> height_input;
 
-  height_feet = height_input / 12;
-  height_inches = height_input % 12;
+  height_feet = height_input / 12; // gives the feet without any decimals as theyare both integers
+  height_inches = height_input % 12; // gets the remainder of height as inches
 
   std::cout << "That person is " << height_feet << '\'' << height_inches << '\"' << std::endl;
 
